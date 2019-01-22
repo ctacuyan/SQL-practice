4d95f575385b7aa9c42da0945c228631c552108b
Carlo T <46909481+ctacuyan@users.noreply.github.com>
2019-01-21 17:53:05 -0800

    Update Create Database Example.sql

--- Create Database Example.sql
+++ Create Database Example.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE tweets;
 USE tweets;
 CREATE TABLE tweets (username varchar(15), content varchar(140), favorites INT);
-INSERT INTO tweets VALUES("Carlo", "Bored hahaha", 50);
-INSERT INTO tweets VALUES("Paolo", "Bored hahaha", 5);
-INSERT INTO tweets VALUES("Shanli", "Bored hahaha", 0);
+INSERT INTO tweets VALUES("Bob", "Bored hahaha", 50);
+INSERT INTO tweets VALUES("John", "Bored hahaha", 5);
+INSERT INTO tweets VALUES("Jack", "Bored hahaha", 0);
 SELECT * FROM tweets;
